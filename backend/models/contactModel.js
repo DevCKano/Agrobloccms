c7e592148c9ca875bdc49b02ed60446b550d7fa3
mubarak23 <mubarakaminu340@gmail.com>
2020-11-05 04:00:37 +0100

    contact model

---
+++ backend/models/contactModel.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const contactSchema = mongoose.Schema({
+	fullname: {
+		type: String,
+		required: true,
+	}
+	email: {
+		type: String,
+		required: true
+	},
+	phone_number:{
+		type: Number,
+		required: true
+	}
+	Anniversary:{
+		type: String
+	},
+	Birthday:{
+		type: Date,
+		required: ttrue
+	},
+	Reminder: [
+		{
+			DateTime: {
+				type: String, required: true
+			},
+			Note: {
+				type: String
+			}
+		}
+	]
+})
