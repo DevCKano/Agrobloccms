e18a96644c580e2c8574c09a7ec37bd8284bd835
mubarak23 <mubarakaminu340@gmail.com>
2020-11-06 19:49:39 +0100

    connect form data

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -4,16 +4,40 @@ import { Form, Button, Row, Col } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const HandleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data } = await axios.post(
+        "http://127.0.0.1:8001/api/auth/AuthLogin",
+        {
+          email,
+          password,
+        }
+      );
+      console.log(data);
+      localStorage.setItem("userLogin", JSON.stringify(data));
+      history.push("home");
+    } catch (error) {
+      alert(error);
+      console.log(error);
+    }
+  };
+
   return (
     <FormContainer>
       <h2>Agbloc CMS Sign In</h2>
-      <Form>
+      <Form onSubmit={HandleLogin}>
         <Form.Group controlId="email">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
             type="email"
             placeholder="Enter Email"
-            value="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
         </Form.Group>
         <Form.Group controlId="password">
@@ -21,7 +45,8 @@ const LoginScreen = () => {
           <Form.Control
             type="password"
             placeholder="Enter Password"
-            value="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
         <Button type="submit" variant="primary">
--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -4,6 +4,35 @@ import { Form, Button, Row, Col } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 
 const RegisterScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const HandleLogin = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage("Password did not match");
+    }
+    try {
+      const { data } = await axios.post(
+        "http://127.0.0.1:8001/api/auth/register",
+        {
+          name,
+          email,
+          password,
+        }
+      );
+      console.log(data);
+      //localStorage.setItem("userLogin", JSON.stringify(data));
+      setMessage(data.message);
+      history.push("/");
+    } catch (error) {
+      //alert(error);
+      console.log(error);
+    }
+  };
   return (
     <FormContainer>
       <h2>Agbloc CMS Sign Up</h2>
@@ -13,7 +42,8 @@ const RegisterScreen = () => {
           <Form.Control
             type="email"
             placeholder="Enter Email"
-            value="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           ></Form.Control>
         </Form.Group>
         <Form.Group controlId="email">
@@ -21,7 +51,8 @@ const RegisterScreen = () => {
           <Form.Control
             type="email"
             placeholder="Enter Email"
-            value="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
         </Form.Group>
         <Form.Group controlId="password">
@@ -29,7 +60,8 @@ const RegisterScreen = () => {
           <Form.Control
             type="password"
             placeholder="Enter Password"
-            value="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
         <Form.Group controlId="password">
@@ -37,7 +69,8 @@ const RegisterScreen = () => {
           <Form.Control
             type="password"
             placeholder="Enter Password"
-            value="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
         <Button type="submit" variant="primary">
