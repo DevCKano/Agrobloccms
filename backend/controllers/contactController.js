377e492a0c703d8beddda2ae4fae2807bb6da87b
mubarak23 <mubarakaminu340@gmail.com>
2020-11-06 18:40:41 +0100

    setup frontend files

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import { connect } from "mongoose";
 import Contact from "../models/contactModel.js";
 
 // @desc    create new Order
@@ -28,4 +29,34 @@ const addContactDetail = asyncHandler(async (req, res) => {
   res.status(201).json(createContact);
 });
 
-export { addContactDetail };
+const updateContact = asyncHandler(async (req, res) => {
+  const {
+    fullname,
+    email,
+    phone_nuumber,
+    Anniversary,
+    Birthday,
+    Reminder,
+  } = req.body;
+  const contact = await Contact.findById(req.body._id);
+  if (contact) {
+    (contact.fullname = fullname),
+      (contact.email = email),
+      (contact.phone_nuumber = phone_nuumber),
+      (contact.Anniversary = Anniversary),
+      (contact.Birthday = Birthday),
+      (contact.Reminder = Birthday);
+    const updatecontact = await contact.save();
+    res.status(200).json("Contact Details Updated Successfully");
+  } else {
+    res.status(400).json("Unabled to update contact");
+  }
+});
+
+const deleteContact = asyncHandler(async (req, res) => {
+  const deleteContact = await Contact.findById(req.params.id);
+  deleteContact.delete();
+  res.status(200).json("Contact Deleted Successfully");
+});
+
+export { addContactDetail, updateContact, deleteContact };
