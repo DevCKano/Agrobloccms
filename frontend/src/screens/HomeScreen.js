7d678a9f37c0c634037df363138e5fb7c6391d35
mubarak23 <mubarakaminu340@gmail.com>
2020-11-07 21:59:51 +0100

    display contact details UI

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -13,8 +13,115 @@ const HomeScreen = () => {
         <Col md={9}>
           <Row>
             <Col md={6}>
-              <Card className="my-3 py-3 rounded">
-                <Card.Body>This is some text within a card body.</Card.Body>
+              <Card style={{ width: "20rem" }} className="card-side">
+                <Card.Body>
+                  <Card.Title>Contact Card</Card.Title>
+                  <Row>
+                    <Col md={6}>
+                      <Card.Text>
+                        <i className="fas fa-user-nurse"></i> Mubarak Aminu
+                      </Card.Text>
+                      <Card.Text>
+                        <i className="fas fa-envelope"></i>mubi@gmail.com
+                      </Card.Text>
+                    </Col>
+                    <Col md={6}>
+                      <Card.Text>
+                        <i className="fas fa-phone-square-alt"></i> 08037287834
+                      </Card.Text>
+                      <Card.Text>
+                        <i className="fas fa-birthday-cake"></i> 01-01-2000
+                      </Card.Text>
+                      <Card.Text>
+                        <i class="fas fa-asterisk"></i> 01-01-2000
+                      </Card.Text>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={6}>
+              <Card style={{ width: "20rem" }} className="card-side">
+                <Card.Body>
+                  <Card.Title>Contact Card</Card.Title>
+                  <Row>
+                    <Col md={6}>
+                      <Card.Text>
+                        <i className="fas fa-user-nurse"></i> Mubarak Aminu
+                      </Card.Text>
+                      <Card.Text>
+                        <i className="fas fa-envelope"></i>mubi@gmail.com
+                      </Card.Text>
+                    </Col>
+                    <Col md={6}>
+                      <Card.Text>
+                        <i className="fas fa-phone-square-alt"></i> 08037287834
+                      </Card.Text>
+                      <Card.Text>
+                        <i className="fas fa-birthday-cake"></i> 01-01-2000
+                      </Card.Text>
+                      <Card.Text>
+                        <i class="fas fa-asterisk"></i> 01-01-2000
+                      </Card.Text>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={6}>
+              <Card style={{ width: "20rem" }} className="card-side">
+                <Card.Body>
+                  <Card.Title>Contact Card</Card.Title>
+                  <Row>
+                    <Col md={6}>
+                      <Card.Text>
+                        <i className="fas fa-user-nurse"></i> Mubarak Aminu
+                      </Card.Text>
+                      <Card.Text>
+                        <i className="fas fa-envelope"></i>mubi@gmail.com
+                      </Card.Text>
+                    </Col>
+                    <Col md={6}>
+                      <Card.Text>
+                        <i className="fas fa-phone-square-alt"></i> 08037287834
+                      </Card.Text>
+                      <Card.Text>
+                        <i className="fas fa-birthday-cake"></i> 01-01-2000
+                      </Card.Text>
+                      <Card.Text>
+                        <i class="fas fa-asterisk"></i> 01-01-2000
+                      </Card.Text>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={6}>
+              <Card style={{ width: "20rem" }} className="card-side">
+                <Card.Body>
+                  <Card.Title>Contact Card</Card.Title>
+                  <Row>
+                    <Col md={6}>
+                      <Card.Text>
+                        <i className="fas fa-user-nurse"></i> Mubarak Aminu
+                      </Card.Text>
+                      <Card.Text>
+                        <i className="fas fa-envelope"></i>mubi@gmail.com
+                      </Card.Text>
+                    </Col>
+                    <Col md={6}>
+                      <Card.Text>
+                        <i className="fas fa-phone-square-alt"></i> 08037287834
+                      </Card.Text>
+                      <Card.Text>
+                        <i className="fas fa-birthday-cake"></i> 01-01-2000
+                      </Card.Text>
+                      <Card.Text>
+                        <i class="fas fa-asterisk"></i> 01-01-2000
+                      </Card.Text>
+                    </Col>
+                  </Row>
+                </Card.Body>
               </Card>
             </Col>
           </Row>
